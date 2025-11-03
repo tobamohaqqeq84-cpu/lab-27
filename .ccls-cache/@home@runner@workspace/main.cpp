605a44540aa6e789024d0a4fc717f3980b191b37
tobam <tobamohaqqeq84@gmail.com>
2025-11-03 04:59:33 +0000

    3rd

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -22,4 +22,35 @@ int clamp_friend(int v);
 bool inc_friend(villagerMap& db, const string& name);
 bool dec_friend(villagerMap& db, const string& name);
 bool search_villager(villagerMap& db, const string& name);
-bool add_villager(villagerMap& db, const string& name, int friendship const string& species, const string& hobby catchphrase);
+bool add_villager(villagerMap& db, const string& name, int friendship, const string& species, const string& catchphrase);
+bool delete_villager(villagerMap& db, const string& name);
+string read_line(const string & prompt);
+int read_int(const string & prompt);
+int menu_3();
+int menu_4();
+
+//----------mai fucntion----------
+
+int main (){
+    villagerMap db;
+    db["Drago"] = villagerInfo(5,  "Alligator", "Snap to It!");
+    db["Kyle"]  = villagerInfo(10, "wolf", "Hubba hubba!");
+    db["Raymond"] = villagerInfo(1, "Cat", "Nice fit");
+
+    cout << "Initial villlager details:\n";
+    print_all(db);
+
+    bool run_m3_menu = false;
+
+    if (run_m3_menu){
+        while(true){
+            int choice = menu_3();
+            if (choice == 4) break;
+            if (choice == 1){
+               string name = read_line("villager name: ");
+               if (inc_friend)
+             }
+        }
+    }
+    
+}
